df383db1c4ce1536d1f50337cfff767117626d40
jmax-dev1998 <johann2maxwell@gmail.com>
2025-06-03 23:07:46 +0700

    add navbar and first code

---
+++ ui-dashboard/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-[#D4EBF8] dark:bg-[#080326] min-h-screen">
+    <Navbar />
+  </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+const Navbar = defineAsyncComponent(() => import('./components/layouts/Navbar.vue'));
+</script>
